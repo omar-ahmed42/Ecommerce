5cb94ceb01093ae973e17425f8725a626d7b922f
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:13:48 +0200

    Create Category.java

---
+++ src/main/java/com/omarahmed42/ecommerce/model/Category.java
@@ -0,0 +1,82 @@
+package com.omarahmed42.ecommerce.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Objects;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Category implements Serializable{
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private int id;
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @OneToMany(mappedBy = "categoryByCategoryId", fetch = FetchType.LAZY)
+    private Collection<CategoryProduct> categoryProductsById;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(int id) {
+        this.id = id;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    public Collection<CategoryProduct> getCategoryProductsById() {
+        return categoryProductsById;
+    }
+
+    public void setCategoryProductsById(Collection<CategoryProduct> categoryProductsById) {
+        this.categoryProductsById = categoryProductsById;
+    }
+}
