8a77e4c760baa2f9cb73bf9c935d57cfabeac9d7
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:20:59 +0200

    Create EcommerceApplication.java

---
+++ src/main/java/com/omarahmed42/ecommerce/EcommerceApplication.java
@@ -0,0 +1,50 @@
+package com.omarahmed42.ecommerce;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.stripe.Stripe;
+
+@EnableAsync
+@EnableWebSecurity
+@EnableJpaAuditing
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class EcommerceApplication {
+
+    @Value("${stripe.api.key}")
+    private String stripeAPIKey;
+
+    @PostConstruct
+    public void setup() {
+        Stripe.apiKey = this.stripeAPIKey;
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        return template;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder bcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
