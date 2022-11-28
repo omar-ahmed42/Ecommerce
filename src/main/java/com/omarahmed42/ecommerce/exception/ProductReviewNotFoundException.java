84051eaa3982ac7db22f106005f608079299bd39
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:16:09 +0200

    Create ProductReviewNotFoundException.java

---
+++ src/main/java/com/omarahmed42/ecommerce/exception/ProductReviewNotFoundException.java
@@ -0,0 +1,7 @@
+package com.omarahmed42.ecommerce.exception;
+
+public class ProductReviewNotFoundException extends RuntimeException {
+    public ProductReviewNotFoundException(String message) {
+        super(message);
+    }
+}
