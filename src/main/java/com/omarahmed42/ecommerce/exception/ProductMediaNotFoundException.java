3308ab4e85f3705d024a681856ed4849ff20140e
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:16:07 +0200

    Create ProductMediaNotFoundException.java

---
+++ src/main/java/com/omarahmed42/ecommerce/exception/ProductMediaNotFoundException.java
@@ -0,0 +1,7 @@
+package com.omarahmed42.ecommerce.exception;
+
+public class ProductMediaNotFoundException extends RuntimeException{
+    public ProductMediaNotFoundException(String message) {
+        super(message);
+    }
+}
