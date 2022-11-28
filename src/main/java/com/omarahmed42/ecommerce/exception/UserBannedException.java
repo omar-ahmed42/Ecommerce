a3b587ac9b014410836a1338af7910dda95abe03
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:15:58 +0200

    Create UserBannedException.java

---
+++ src/main/java/com/omarahmed42/ecommerce/exception/UserBannedException.java
@@ -0,0 +1,7 @@
+package com.omarahmed42.ecommerce.exception;
+
+public class UserBannedException extends RuntimeException {
+    public UserBannedException(String message) {
+        super(message);
+    }
+}
