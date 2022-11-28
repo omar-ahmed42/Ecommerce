34d6c8a81ceb1b58cb956068c8fb1c8bdfb2004d
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:20:13 +0200

    Create CustomerService.java

---
+++ src/main/java/com/omarahmed42/ecommerce/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.omarahmed42.ecommerce.service;
+
+import com.omarahmed42.ecommerce.model.Customer;
+
+public interface CustomerService {
+    public void addCustomer(Customer customer);
+    public void deleteCustomer(byte[] id);
+    public void updateCustomer(Customer customer);
+}
