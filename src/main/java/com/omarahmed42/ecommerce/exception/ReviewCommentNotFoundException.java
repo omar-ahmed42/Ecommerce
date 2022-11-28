595ee938c203cac30448371d1d35aaf1677ab58d
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:16:10 +0200

    Create ReviewCommentNotFoundException.java

---
+++ src/main/java/com/omarahmed42/ecommerce/exception/ReviewCommentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.omarahmed42.ecommerce.exception;
+
+public class ReviewCommentNotFoundException extends RuntimeException {
+    public ReviewCommentNotFoundException(String message) {
+        super(message);
+    }
+}
