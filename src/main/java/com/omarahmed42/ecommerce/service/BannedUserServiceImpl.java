bf4938dc237ed5e79931f2b20af7a55e92db38a5
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-11-28 17:20:53 +0200

    Create BannedUserServiceImpl.java

---
+++ src/main/java/com/omarahmed42/ecommerce/service/BannedUserServiceImpl.java
@@ -0,0 +1,81 @@
+package com.omarahmed42.ecommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.omarahmed42.ecommerce.exception.BannedUserNotFoundException;
+import com.omarahmed42.ecommerce.exception.UserNotFoundException;
+import com.omarahmed42.ecommerce.model.BannedUser;
+import com.omarahmed42.ecommerce.repository.BannedUserRepository;
+import com.omarahmed42.ecommerce.repository.UserRepository;
+
+@Service
+public class BannedUserServiceImpl implements BannedUserService {
+
+    private final BannedUserRepository bannedUserRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public BannedUserServiceImpl(BannedUserRepository bannedUserRepository, UserRepository userRepository) {
+        this.bannedUserRepository = bannedUserRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    @Override
+    public void addBannedUser(BannedUser bannedUser) {
+        userRepository
+                .findById(bannedUser.getUserId())
+                .ifPresentOrElse(
+                        presentUser -> {
+                            presentUser.setBanned(true);
+                            userRepository.save(presentUser);
+                            bannedUserRepository.save(bannedUser);
+                        },
+                        () -> {
+                            throw new UserNotFoundException("User not found");
+                        });
+    }
+
+    @Transactional
+    @Override
+    public void deleteBannedUser(byte[] id) {
+        bannedUserRepository
+                .findById(id)
+                .ifPresentOrElse(
+                        presentBannedUser -> {
+                            userRepository
+                                    .findById(presentBannedUser.getUserId())
+                                    .ifPresent(presentUser -> {
+                                        presentUser.setBanned(false);
+                                        userRepository.save(presentUser);
+                                    });
+                            bannedUserRepository.delete(presentBannedUser);
+                        },
+                        () -> {
+                            throw new BannedUserNotFoundException("Banned user not found");
+                        });
+    }
+
+    @Transactional
+    @Override
+    public void updateBannedUser(BannedUser bannedUser) {
+        bannedUserRepository
+                .findById(bannedUser.getUserId())
+                .ifPresentOrElse(
+                        presentBannedUser -> {
+                            bannedUserRepository.save(bannedUser);
+                        },
+                        () -> {
+                            throw new BannedUserNotFoundException("Banned user not found");
+                        });
+    }
+
+    @Override
+    public BannedUser getBannedUser(byte[] id) {
+        return bannedUserRepository
+                .findById(id)
+                .orElseThrow(() -> new BannedUserNotFoundException("Banned user not found"));
+    }
+}
