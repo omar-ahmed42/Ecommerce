32f20f17b68c7ceaadf388d2ee89acdff20abe2f
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-04-30 19:14:27 +0300

    Refactor payment service & controller

--- src/main/java/com/omarahmed42/ecommerce/service/PaymentService.java
+++ src/main/java/com/omarahmed42/ecommerce/service/PaymentService.java
@@ -1,13 +1,16 @@
 package com.omarahmed42.ecommerce.service;
 
 import com.omarahmed42.ecommerce.model.Payment;
-
-import java.util.Optional;
+import com.stripe.model.PaymentIntent;
 
 public interface PaymentService {
     void addPayment(Payment payment);
+
     void deletePayment(String paymentId);
+
     void updatePayment(Payment payment);
+
     Payment getPayment(String id);
-    Optional<Payment> findPayment(String id);
+
+    void handlePaymentIntent(String eventType, PaymentIntent paymentIntent);
 }
--- src/main/java/com/omarahmed42/ecommerce/service/PaymentServiceImpl.java
+++ src/main/java/com/omarahmed42/ecommerce/service/PaymentServiceImpl.java
@@ -1,59 +1,153 @@
 package com.omarahmed42.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.omarahmed42.ecommerce.DTO.OrderDetailsDTO;
+import com.omarahmed42.ecommerce.enums.PaymentStatus;
+import com.omarahmed42.ecommerce.enums.Status;
 import com.omarahmed42.ecommerce.exception.PaymentNotFoundException;
+import com.omarahmed42.ecommerce.model.Orders;
 import com.omarahmed42.ecommerce.model.Payment;
+import com.omarahmed42.ecommerce.model.Product;
+import com.omarahmed42.ecommerce.model.ProductItem;
+import com.omarahmed42.ecommerce.repository.OrderRepository;
 import com.omarahmed42.ecommerce.repository.PaymentRepository;
+import com.omarahmed42.ecommerce.repository.ProductRepository;
+import com.stripe.model.PaymentIntent;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PaymentServiceImpl.class);
+    private final OrdersService ordersService;
+    private final OrderRepository orderRepository;
     private final PaymentRepository paymentRepository;
+    private final ProductRepository productRepository;
 
-    public PaymentServiceImpl(PaymentRepository paymentRepository) {
-        this.paymentRepository = paymentRepository;
-    }
+    private static final String ORDER_ID = "orderId";
 
-    @Transactional
     @Override
+    @Transactional
     public void addPayment(Payment payment) {
         paymentRepository.save(payment);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public void deletePayment(String paymentId) {
         paymentRepository.deleteById(paymentId);
     }
 
-    @Transactional
     @Override
+    @Transactional
     public void updatePayment(Payment payment) {
         paymentRepository
                 .findById(payment.getPaymentIntentId())
                 .ifPresentOrElse(
-                        presentPayment -> paymentRepository.save(payment)
-                        ,
-                        () -> {
-                            throw new PaymentNotFoundException("Payment not found");
-                        }
-                );
+                        presentPayment -> paymentRepository.save(payment),
+                        PaymentNotFoundException::new);
     }
 
-    @Transactional
     @Override
+    @Transactional(readOnly = true)
     public Payment getPayment(String id) {
         return paymentRepository
                 .findById(id)
-                .orElseThrow(() -> new PaymentNotFoundException("Payment not found"));
+                .orElseThrow(PaymentNotFoundException::new);
     }
 
-    public Optional<Payment> findPayment(String id){
-        return paymentRepository
-                .findById(id);
+    @Override
+    @Transactional
+    public void handlePaymentIntent(String eventType, PaymentIntent paymentIntent) {
+        switch (eventType) {
+            case "payment_intent.created":
+                logger.info("Payment for {} created.", paymentIntent.getAmount());
+                handlePaymentCreated(paymentIntent);
+                break;
+            case "payment_intent.succeeded":
+                logger.info("Payment for {} succeeded.", paymentIntent.getAmount());
+                handlePaymentIntentSucceeded(paymentIntent);
+                break;
+            case "payment_intent.payment_failed":
+                logger.info("Payment for {} failed.", paymentIntent.getId());
+                handlePaymentIntentFailed(paymentIntent);
+                break;
+            default:
+                logger.warn("Unhandled event type: {}", eventType);
+                break;
+        }
+    }
+
+    private void handlePaymentCreated(PaymentIntent paymentIntent) {
+        if (paymentRepository.existsById(paymentIntent.getId()))
+            return;
+
+        Map<String, String> metadata = paymentIntent.getMetadata();
+        UUID orderId = UUID.fromString(metadata.get(ORDER_ID));
+        Payment payment = new Payment(paymentIntent.getId(), paymentIntent.getAmount(), PaymentStatus.CREATED, orderId);
+        paymentRepository.save(payment);
+    }
+
+    private void handlePaymentIntentSucceeded(PaymentIntent paymentIntent) {
+        Optional<Payment> retrievedPayment = paymentRepository.findById(paymentIntent.getId());
+        if (retrievedPayment.isEmpty()) {
+            return;
+        }
+
+        Map<String, String> metadata = paymentIntent.getMetadata();
+        UUID orderId = UUID.fromString(metadata.get(ORDER_ID));
+        ordersService
+                .updateOrderPartially(orderId, OrderDetailsDTO
+                        .builder()
+                        .status(Status.COMPLETED)
+                        .build());
+        Payment payment = retrievedPayment.get();
+        payment.setPaymentStatus(PaymentStatus.COMPLETED);
+        paymentRepository.save(payment);
+    }
+
+    private void handlePaymentIntentFailed(PaymentIntent paymentIntent) {
+        Optional<Payment> retrievedPayment = paymentRepository.findById(paymentIntent.getId());
+        if (retrievedPayment.isEmpty()) {
+            return;
+        }
+        Map<String, String> metadata = paymentIntent.getMetadata();
+        UUID orderId = UUID.fromString(metadata.get(ORDER_ID));
+        ordersService
+                .updateOrderPartially(orderId, OrderDetailsDTO
+                        .builder()
+                        .status(Status.FAILED)
+                        .build());
+
+        Payment payment = retrievedPayment.get();
+        payment.setPaymentStatus(PaymentStatus.FAILED);
+        paymentRepository.save(payment);
+        new Thread(() -> updateProductStock(orderId)).start();
+    }
+
+    private void updateProductStock(UUID orderId) {
+        Orders order = orderRepository.getReferenceById(orderId);
+        List<ProductItem> productItemsById = order.getProductItemsById();
+        List<Product> products = new ArrayList<>(productItemsById.size());
+        for (ProductItem productItem : productItemsById) {
+            Product product = productItem.getProductByProductId();
+            int stock = product.getStock() + productItem.getQuantity();
+            product.setStock(stock);
+            products.add(product);
+        }
+
+        productRepository.saveAllAndFlush(products);
     }
 }
