9b15f612b58dcd2e6e5dfb205c640e86dd42e43c
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-05-07 19:52:30 +0300

    Fix a bug that prevented product's categories from being retrieved

--- src/main/java/com/omarahmed42/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/omarahmed42/ecommerce/repository/ProductRepository.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.omarahmed42.ecommerce.model.Category;
@@ -12,5 +14,6 @@ import com.omarahmed42.ecommerce.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
-    List<Category> findCategoriesById(UUID productId);
+    @Query("SELECT p.categories FROM Product p WHERE p.id = :productId")
+    List<Category> findCategoriesById(@Param("productId") UUID productId);
 }
