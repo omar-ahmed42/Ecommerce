47faef8e78621121963536fd6ac9bc72809413cb
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-05-23 16:23:44 +0300

    Fix a bug that caused recursive directory creation to fail
    - Fix a bug that caused recursive directory creation to fail throwing an exception

--- src/main/java/com/omarahmed42/ecommerce/exception/UnsupportedFileExtensionException.java
+++ src/main/java/com/omarahmed42/ecommerce/exception/UnsupportedFileExtensionException.java
@@ -1,6 +1,6 @@
 package com.omarahmed42.ecommerce.exception;
 
-public class UnsupportedFileExtensionException extends InvalidInputException {
+public class UnsupportedFileExtensionException extends ValidationException {
     private static final String UNSUPPORTED_FILE_EXTENSION = "Unsupported file extension";
 
     public UnsupportedFileExtensionException(String message) {
